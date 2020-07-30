5103b1280c9ef0459a39d4a17c5b129289377b17
huyuansong <2455428161@qq.com>
2020-07-30 18:13:01 +0800

    equals的标准写法

--- 02-Linear-Search/05-Using-Custom-Class/src/Student.java
+++ 02-Linear-Search/05-Using-Custom-Class/src/Student.java
@@ -9,13 +9,13 @@ public class Student {
     @Override
     public boolean equals(Object student){
 
-        if(this == student)
+        if(this == student)  // 地址相同，则对象相同
             return true;
 
-        if(student == null)
+        if(student == null)  // 对象为空，则不相等
             return false;
 
-        if(this.getClass() != student.getClass())
+        if(this.getClass() != student.getClass())  // 两个对象不是同一个类的实例，不相等
             return false;
 
         Student another = (Student)student;
