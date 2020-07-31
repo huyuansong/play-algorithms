95a1772526841c88c2be578b0c33b2765bf20ca8
huyuansong <2455428161@qq.com>
2020-07-31 16:39:29 +0800

    语义维护

--- 06-Stacks-and-Queues/06-Loop-Queue/src/LoopQueue.java
+++ 06-Stacks-and-Queues/06-Loop-Queue/src/LoopQueue.java
@@ -7,7 +7,7 @@ public class LoopQueue<E> implements Queue<E> {
                         // 这个问题可能会比大家想象的要难一点点：）
 
     public LoopQueue(int capacity){
-        data = (E[])new Object[capacity + 1];
+        data = (E[])new Object[capacity + 1]; // 用户期待能装入capacity个元素，在实现的原理上，需要空出一个位置区分头和尾
         front = 0;
         tail = 0;
         size = 0;
@@ -18,12 +18,12 @@ public class LoopQueue<E> implements Queue<E> {
     }
 
     public int getCapacity(){
-        return data.length - 1;
+        return data.length - 1;  // 有一个空余的位置，实际长度需要-1
     }
 
     @Override
     public boolean isEmpty(){
-        return front == tail;
+        return front == tail;   // tail指针指向的是最后一个元素的下一个位置，两个指针重叠表示为空，则为满的时候只能让数组空出来一个位置
     }
 
     @Override
