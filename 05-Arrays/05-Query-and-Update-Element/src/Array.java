96034021906641eb60afec6919ca4c1cb99c9a4a
huyuansong <2455428161@qq.com>
2020-07-31 10:37:08 +0800

    修改打印格式

--- 05-Arrays/05-Query-and-Update-Element/src/Array.java
+++ 05-Arrays/05-Query-and-Update-Element/src/Array.java
@@ -75,7 +75,7 @@ public class Array {
     public String toString(){
 
         StringBuilder res = new StringBuilder();
-        res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
+        res.append(String.format("Array: size = %d , capacity = %d ", size, data.length));
         res.append('[');
         for(int i = 0 ; i < size ; i ++){
             res.append(data[i]);
